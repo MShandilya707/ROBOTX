e36430855bd2046959f8cb9aa0b3e8b306d07977
MShandilya707 <146753545+MShandilya707@users.noreply.github.com>
2024-11-05 22:20:32 +0800

    tried to get it to do one at a time

--- hbt_socket.py
+++ hbt_socket.py
@@ -33,6 +33,8 @@ class HeartbeatPublisher:
 
         # Initialize and connect the TCP socket using the hostname
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         try:
             self.sock.connect((self.server_hostname, self.server_port))
             rospy.loginfo(f"Connected to server at {self.server_hostname}:{self.server_port}")
